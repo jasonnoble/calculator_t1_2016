19be341ea3e3bb20dd9293761cf7b0b5ff1d3b8e
Jason Noble <github+jasonn@jasonnoble.org>
2016-03-14 20:22:07 -0600

    Refactored

--- app/controllers/calculator_controller.rb
+++ app/controllers/calculator_controller.rb
@@ -1,25 +1,17 @@
 class CalculatorController < ApplicationController
-  def plus
+  def do_some_maths
     @number_1 = params[:number_1].to_i
     @number_2 = params[:number_2].to_i
-    @result = @number_1 + @number_2
-  end
 
-  def minus
-    @number_1 = params[:number_1].to_i
-    @number_2 = params[:number_2].to_i
-    @result = @number_1 - @number_2
-    end
-
-  def times
-    @number_1 = params[:number_1].to_i
-    @number_2 = params[:number_2].to_i
-    @result = @number_1 * @number_2
-  end
-
-  def div
-    @number_1 = params[:number_1].to_i
-    @number_2 = params[:number_2].to_i
-    @result = @number_1 / @number_2
+    (@result, @operator) = case params[:operator]
+              when 'plus' then
+                [@number_1 + @number_2, '+']
+              when 'minus' then
+                [@number_1 - @number_2, '-']
+              when 'times' then
+                [@number_1 * @number_2, '*']
+              when 'div' then
+                [@number_1 / @number_2, '/']
+              end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,3 @@
 Rails.application.routes.draw do
-  get 'calculator/:number_1/plus/:number_2' => 'calculator#plus'
-  get 'calculator/:number_1/minus/:number_2' => 'calculator#minus'
-  get 'calculator/:number_1/times/:number_2' => 'calculator#times'
-  get 'calculator/:number_1/div/:number_2' => 'calculator#div'
+  get 'calculator/:number_1/:operator/:number_2' => 'calculator#do_some_maths'
 end
